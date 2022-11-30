3647e403aaccf50cd2b610da4811fa331e616733
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-11-30 18:46:52 +0100

    TASK DONE

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-def fizzbuzz():
-    """Prints numbers 1 to 100 separated by a space
-    For multiples of three, print Fizz instead of the number
-    For multiples of five, print Buzz instead of the number
-    For multiples of three and five, print FizzBuzz instead of the number
-    """
-    for i in range(1, 101):
-        if (i % 15 == 0):
-            print("FizzBuzz ", end="")
-        elif (i % 3 == 0):
-            print("Fizz ", end="")
-        elif (i % 5 == 0):
-            print("Buzz ", end="")
-        else:
-            print("{:d} ".format(i), end="")
+for i in range(97, 123):
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{}".format(chr(i)), end='')
