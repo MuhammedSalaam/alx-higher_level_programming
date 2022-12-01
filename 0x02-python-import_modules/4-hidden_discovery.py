8545518896795a33f63f0ce9a820bcf76d3150df
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-12-01 07:40:45 +0100

    TASK DONE

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,8 @@
-bbbhbhb
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Prints all the names defined by
+    the complied module hiden_4.pyc"""
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
