bc065494e908f2cca16eefa8df85187f14c124eb
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-12-01 07:39:49 +0100

    TASK DONE

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
-bdhbhbhjh
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Prints the result of the addition of all arguments"""
+    import sys
+    adds = 0
+    num_arg = sys.argv
+
+    for i in range(1, len(num_arg)):
+        adds += int(num_arg[i])
+    print(adds)
