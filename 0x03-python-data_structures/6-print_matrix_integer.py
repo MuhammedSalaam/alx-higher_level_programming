3d3c176b1ce7b89cc1ca86791508c2ff2346d601
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-12-04 17:04:06 +0100

    TASK DONE

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,6 @@
-msnjmnsj
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers."""
+
+    for rows in matrix:
+        print(" ".join("{:d}".format(cols) for cols in rows))
