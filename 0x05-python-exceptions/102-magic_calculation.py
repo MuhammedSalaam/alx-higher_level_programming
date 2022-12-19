bcdae2b7b9934582aab4bacba8746060ea48dda5
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-12-19 11:03:11 +0100

    TASK DONE

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,1 +1,14 @@
-ww3ewwerw
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    """Function that does exactly the same as Python bytecode"""
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too Far")
+            else:
+                result += (a ** b) / i
+        except (Exception):
+            result = b + a
+            break
+    return result
