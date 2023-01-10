d47b71a78a4dd00f1672cab6d607413b4a9922cc
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2023-01-10 08:14:43 +0100

    TASK DONE

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
-sjjj
+#!/usr/bin/python3
+"""Improves Geometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        """Raises an exception"""
+        raise Exception('area() is not implemented')
