5a186690724eb7301b7455c2645f6e0aaa9bb61c
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2023-01-10 08:56:23 +0100

    TASK DONE

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,8 @@
-hshshs
+#!/usr/bin/python3
+"""Reads files"""
+
+
+def read_file(filename=""):
+    """Reads a file and prints its content"""
+    with open(filename, encoding="utf-8") as txt_file:
+        print(txt_file.read(), end="")
