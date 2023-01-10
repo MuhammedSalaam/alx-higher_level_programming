e37030a22046fa37c9d0b5c33b2a554a11cee472
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2023-01-10 08:08:45 +0100

    TASK DONE

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,9 @@
-nnnw
+#!/usr/bin/python3
+"""Exact same object"""
+
+
+def is_same_class(obj, a_class):
+    """Returns 'True'' if the object is exactly
+    an instance of the specified class ; otherwise False
+    """
+    return type(obj) == a_class
