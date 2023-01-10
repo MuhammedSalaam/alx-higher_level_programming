d4a445a498ecd404d79e0d09ce77623d0b1abae7
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2023-01-10 08:21:06 +0100

    TASK DONE

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,13 @@
-as
+#!/usr/bin/python3
+"""Class My Imteger"""
+
+
+class MyInt(int):
+    """Class Myint that inherits from int"""
+    def __eq__(self, val):
+        """what was != is now =="""
+        return int(self) != val
+
+    def __ne__(self, val):
+        """what was == is now !="""
+        return int(self) == val
