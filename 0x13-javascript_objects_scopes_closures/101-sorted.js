c3b99d9eea9f7b7062fdb98ea113caef60e86987
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2023-03-15 22:18:51 +0100

    TASK DONE

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,11 @@
-fjkfkfkfk
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const userId in dict) {
+  const occurrences = dict[userId];
+  if (!newDict[occurrences]) {
+    newDict[occurrences] = [];
+  }
+  newDict[occurrences].push(userId);
+}
+console.log(newDict);
