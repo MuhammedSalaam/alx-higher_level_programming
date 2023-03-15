1841b52956dfdb9ed5d372bb790eb82f5612de02
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2023-03-15 22:55:28 +0100

    TASK DONE

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,9 @@
-hjjj
+#!/usr/bin/node
+const size = Number(process.argv[2]);
+if (size) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
